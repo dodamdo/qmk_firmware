e86fe73be3c3f189bb4e18b20bd597dd91b5dd74
최광필 <it_hing159@naver.com>
2025-07-27 02:09:51 +0900

    Update rules.mk

--- keyboards/silakka54/rules.mk
+++ keyboards/silakka54/rules.mk
@@ -1,7 +1,10 @@
-# 기본 빌드 설정
-SRC += \
-    matrix.c
+# keyboards/silakka54/rules.mk
 
+# MCU 설정 (RP2040)
+MCU = RP2040
+
+# Build options
 BOOTLOADER = rp2040
+LAYOUTS = LAYOUT
 VIA_ENABLE = yes
 LTO_ENABLE = yes
